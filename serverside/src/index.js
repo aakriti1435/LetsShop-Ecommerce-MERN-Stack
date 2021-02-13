39bb292058b957c3315ada380fd9fe8fe0f31dd5
Aakriti Gupta <65544777+aakriti1435@users.noreply.github.com>
2021-02-13 10:44:13 +0530

    Created Server

--- serverside/src/index.js
+++ serverside/src/index.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const env = require('dotenv');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 env.config();
+
+
+//Middlewares
+app.use(express.json());
+// app.use(bodyParser());
+
+
+//API Calls
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Hello Aakriti',
+    })
+});
+
+app.post('/data', (req, res, next) => {
+    res.status(200).json({
+        message: req.body,
+    })
+});
 
 
 app.listen(process.env.PORT, () => {
