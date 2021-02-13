07742c6fe4d867f5330be04d0ee0c61745a1c01d
Aakriti Gupta <65544777+aakriti1435@users.noreply.github.com>
2021-02-13 10:22:02 +0530

    Running backend Server

---
+++ serverside/src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const env = require('dotenv');
+
+
+const app = express();
+env.config();
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on PORT ${process.env.PORT}`);
+});
