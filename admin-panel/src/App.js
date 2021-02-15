6c38aed94b5f9c4700d193907b2a3e5b1dfa2658
Aakriti Gupta <65544777+aakriti1435@users.noreply.github.com>
2021-02-15 14:32:59 +0530

    AdminApp - Routes for Pages

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
-import { Jumbotron } from "react-bootstrap";
-import Layout from './components/Layout/Layout';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './containers/Home/Home';
+import SignIn from './containers/SignIn/SignIn';
+import SignUp from './containers/SignUp/SignUp';
 
 function App() {
   return (
     <div className="app">
-      <Layout>
-        <h1>admin</h1>
-      </Layout>
+      <Router>
+        <Switch>
+          <Route exact path='/signIn' component={SignIn} />
+          <Route exact path='/signUp' component={SignUp} />
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
