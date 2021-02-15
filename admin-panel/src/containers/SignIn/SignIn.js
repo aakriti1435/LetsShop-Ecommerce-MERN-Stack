2d17290a67b71778411d783afb8c8f15ae0140a3
Aakriti Gupta <65544777+aakriti1435@users.noreply.github.com>
2021-02-15 15:19:27 +0530

    AP- SignIn

--- admin-panel/src/containers/SignIn/SignIn.js
+++ admin-panel/src/containers/SignIn/SignIn.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import Layout from '../../components/Layout/Layout';
 
 function SignIn() {
     return (
-        <div>
-
-        </div>
+        <Layout>
+            <Container>
+                <Row style={{ marginTop: '50px' }}>
+                    <Col md={{ span: 6, offset: 3 }}>
+                        <Form>
+                            <Input label="Email" placeholder="Email" type="email" />
+                            <Input label="Password" placeholder="Password" type="password" />
+                            <Button variant="primary" type="submit">Submit</Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+        </Layout>
     )
 }
 
