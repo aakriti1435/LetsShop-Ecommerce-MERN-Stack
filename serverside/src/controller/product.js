df0ac6140238729d3515cd9571cb3f5978e04a37
Aakriti Gupta <65544777+aakriti1435@users.noreply.github.com>
2021-02-16 18:22:27 +0530

    Uplodaing Multiple images

--- serverside/src/controller/product.js
+++ serverside/src/controller/product.js
@@ -1,5 +1,25 @@
 import Product from '../models/product.js';
+import slugify from 'slugify';
 
 export const createProduct = (req, res) => {
-    res.status(200).json({ file: req.file, body: req.body });
+
+    const { name, price, description, category, createdBy } = req.body;
+
+    const product = new Product({
+        name: name,
+        slug: slugify(name),
+        price,
+        description,
+        productPictures,
+        category,
+        createdBy: req.user._id
+    });
+
+    product.save().exec((error, product) => {
+        if (error)
+            return res.status(400).json({ error });
+
+        if (product)
+            return res.status(201).json({ product });
+    });
 };
--- serverside/src/routes/product.js
+++ serverside/src/routes/product.js
@@ -2,11 +2,21 @@ import express from 'express';
 import { requireSignIn, adminMiddleware } from '../common-middlewares/index.js';
 import { createProduct } from '../controller/product.js';
 import multer from 'multer';
+import shortid from 'shortid';
+import path from 'path';
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, './src/uploads/')
+    },
+    filename: function(req, file, cb) {
+        cb(null, shortid.generate() + '-' + file.originalname)
+    }
+});
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ storage });
 
 const router = express.Router();
 
-router.post('/product/create', upload.single('productPicture'), createProduct);
+router.post('/product/create', upload.array('productPicture'), createProduct);
 
 export default router;
