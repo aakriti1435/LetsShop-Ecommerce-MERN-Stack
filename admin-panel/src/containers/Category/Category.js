7d852c3feca1d84832201c2ca3f2a817964dc318
Aakriti Gupta <65544777+aakriti1435@users.noreply.github.com>
2021-02-21 22:47:18 +0530

    Displaying Categories

--- admin-panel/src/containers/Category/Category.js
+++ admin-panel/src/containers/Category/Category.js
@@ -1,17 +1,33 @@
 import React, { useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getAllCategories } from '../../actions/actions';
 import Layout from '../../components/Layout/Layout';
 import './Category.css';
 
 function Category() {
 
+    const category = useSelector((state) => state.category)
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getAllCategories());
     }, []);
+
+    const renderCategories = (categories) => {
+        console.log(">>>>", categories);
+        let myCategories = [];
+        for (let category of categories) {
+            myCategories.push(
+                <li key={category._id}>
+                    {category.name}
+                    {category.children.length > 0 ? (<ul>{renderCategories(category.children)}</ul>) : null}
+                </li>
+            )
+        };
+
+        return myCategories;
+    }
 
     return (
         <Layout sidebar>
@@ -25,6 +41,13 @@ function Category() {
                                 <button>Add</button>
                             </div>
                         </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={12}>
+                        <ul>
+                            {renderCategories(category.categories)}
+                        </ul>
                     </Col>
                 </Row>
             </Container>
