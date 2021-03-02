39bd5a99d912e8ee34fdfe7450691314b9a494cf
aakriti1435 <write10aakriti07@gmail.com>
2021-03-02 00:03:01 -0800

    Fetching Products by Slug

--- serverside/src/controller/product.js
+++ serverside/src/controller/product.js
@@ -1,16 +1,72 @@
-import Product from '../models/product.js';
-import slugify from 'slugify';
+import Product from "../models/product.js";
+import slugify from "slugify";
+import Category from "../models/category.js";
+
+export const getProductsBySlug = (req, res) => {
+    const { slug } = req.params;
+
+    Category.findOne({ slug: slug })
+        .select("_id")
+        .exec((error, category) => {
+            if (error) return res.status(400).json({ error });
+
+            if (category) {
+                Product.find({
+                    category: category._id,
+                }).exec((error, products) => {
+                    if (error) return res.status(400).json({ error });
+
+                    if (products.length > 0) {
+                        res.status(200).json({
+                            products,
+                            productsByPrice: {
+                                under5k: products.filter(
+                                    (product) => product.price <= 5000
+                                ),
+                                under10k: products.filter(
+                                    (product) =>
+                                        product.price > 5000 &&
+                                        product.price <= 10000
+                                ),
+                                under15k: products.filter(
+                                    (product) =>
+                                        product.price > 10000 &&
+                                        product.price <= 15000
+                                ),
+                                under20k: products.filter(
+                                    (product) =>
+                                        product.price > 15000 &&
+                                        product.price <= 20000
+                                ),
+                                under30k: products.filter(
+                                    (product) =>
+                                        product.price > 20000 &&
+                                        product.price <= 30000
+                                ),
+                            },
+                        });
+                    }
+                });
+            }
+        });
+};
 
 export const createProduct = (req, res) => {
-
-    const { name, price, description, category, quantity, createdBy } = req.body;
+    const {
+        name,
+        price,
+        description,
+        category,
+        quantity,
+        createdBy,
+    } = req.body;
 
     let productPictures = [];
     if (req.files.length > 0) {
         productPictures = req.files.map((file) => {
-            return { img: file.filename }
-        })
-    };
+            return { img: file.filename };
+        });
+    }
 
     const product = new Product({
         name: name,
@@ -20,14 +76,12 @@ export const createProduct = (req, res) => {
         description,
         productPictures,
         category,
-        createdBy: req.user._id
+        createdBy: req.user._id,
     });
 
     product.save((error, product) => {
-        if (error)
-            return res.status(400).json({ error });
+        if (error) return res.status(400).json({ error });
 
-        if (product)
-            return res.status(201).json({ product });
+        if (product) return res.status(201).json({ product });
     });
 };
