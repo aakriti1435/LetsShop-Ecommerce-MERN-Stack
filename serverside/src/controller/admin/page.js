4e27f074cfff3719038a02ee64544d037c6e709d
aakriti1435 <write10aakriti07@gmail.com>
2021-03-03 23:59:42 +0530

    Create page API

--- serverside/src/controller/admin/page.js
+++ serverside/src/controller/admin/page.js
@@ -17,5 +17,30 @@ export const createPage = (req, res) => {
         }));
     }
 
+    req.body.createdBy = req.user._id;
+
+    Page.findOne({ category: req.body.category }).exec((error, page) => {
+        if (error) return res.status(400).json({ error });
+        if (page) {
+            Page.findOneAndUpdate(
+                { category: req.body.category },
+                req.body
+            ).exec((error, updatedPage) => {
+                if (error) return res.status(400).json({ error });
+                if (updatedPage) {
+                    return res.status(201).json({ page: updatedPage });
+                }
+            });
+        } else {
+            const page = new Page(req.body);
+            page.save((error, page) => {
+                if (error) return res.status(400).json({ error });
+                if (page) {
+                    return res.status(201).json({ page });
+                }
+            });
+        }
+    });
+
     res.status(200).json({ body: req.body });
 };
