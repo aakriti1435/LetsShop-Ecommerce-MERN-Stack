124371c2008b70075fed93c77165e3e45d810a15
aakriti1435 <write10aakriti07@gmail.com>
2021-03-04 12:40:12 +0530

    PRoductPAge

--- clientside/src/actions/product.js
+++ clientside/src/actions/product.js
@@ -3,25 +3,51 @@ import { productConstants } from "./constants";
 
 export const getProductsBySlug = (slug) => {
     return async (dispatch) => {
-        const res = await axios.get(`/products/${slug}`);
-        console.log(res);
+        try {
+            dispatch({ type: productConstants.GET_PRODUCTS_BY_SLUG_REQUEST });
 
-        if (res.status === 200) {
-            dispatch({
-                type: productConstants.GET_PRODUCTS_BY_SLUG,
-                payload: res.data,
-            });
+            const res = await axios.get(`/products/${slug}`);
+            console.log(res);
+
+            if (res.status === 200) {
+                dispatch({
+                    type: productConstants.GET_PRODUCTS_BY_SLUG_SUCCESS,
+                    payload: res.data,
+                });
+            } else {
+                dispatch({
+                    type: productConstants.GET_PRODUCTS_BY_SLUG_FAILURE,
+                    payload: { error: res.data.error },
+                });
+            }
+        } catch (error) {
+            console.log(error);
         }
     };
 };
 
 export const getProductPage = (payload) => {
     return async (dispatch) => {
-        const { cid, type } = payload.params;
-        const res = await axios.get(`/page/${cid}/${type}`);
-        console.log(res);
-        if (res.status === 200) {
-        } else {
+        try {
+            dispatch({ type: productConstants.GET_PRODUCT_PAGE_REQUEST });
+
+            const { cid, type } = payload.params;
+            const res = await axios.get(`/page/${cid}/${type}`);
+            console.log(res);
+            if (res.status === 200) {
+                const { page } = res.data;
+                dispatch({
+                    type: productConstants.GET_PRODUCT_PAGE_SUCCESS,
+                    payload: { page },
+                });
+            } else {
+                dispatch({
+                    type: productConstants.GET_PRODUCT_PAGE_FAILURE,
+                    payload: { error: res.data.error },
+                });
+            }
+        } catch (error) {
+            console.log(error);
         }
     };
 };
--- clientside/src/reducers/product.js
+++ clientside/src/reducers/product.js
@@ -2,6 +2,9 @@ import { productConstants } from "../actions/constants";
 
 const initialState = {
     products: [],
+    loading: false,
+    error: null,
+    message: "",
     productsByPrice: {
         under5k: [],
         under10k: [],
@@ -9,17 +12,60 @@ const initialState = {
         under20k: [],
         under30k: [],
     },
+    pageRequest: false,
+    page: {},
 };
 
 export default (state = initialState, action) => {
     switch (action.type) {
-        case productConstants.GET_PRODUCTS_BY_SLUG:
+        case productConstants.GET_PRODUCTS_BY_SLUG_REQUEST:
+            state = {
+                ...state,
+                loading: true,
+            };
+            break;
+
+        case productConstants.GET_PRODUCTS_BY_SLUG_SUCCESS:
             state = {
                 ...state,
                 products: action.payload.products,
                 productsByPrice: {
                     ...action.payload.productsByPrice,
                 },
+            };
+            break;
+
+        case productConstants.GET_PRODUCTS_BY_SLUG_FAILURE:
+            state = {
+                ...state,
+                error: action.payload.error,
+                loading: false,
+            };
+            break;
+
+        case productConstants.GET_PRODUCT_PAGE_REQUEST:
+            state = {
+                ...state,
+                loading: true,
+                pageRequest: true,
+            };
+            break;
+
+        case productConstants.GET_PRODUCT_PAGE_SUCCESS:
+            state = {
+                ...state,
+                page: action.payload.page,
+                loading: false,
+                pageRequest: false,
+            };
+            break;
+
+        case productConstants.GET_PRODUCT_PAGE_FAILURE:
+            state = {
+                ...state,
+                error: action.payload.error,
+                loading: false,
+                pageRequest: false,
             };
             break;
     }
