051350ec2998e6756a966ce9fe15b4ea2c37cccd
aakriti1435 <write10aakriti07@gmail.com>
2021-03-06 00:55:11 +0530

    Address

--- clientside/src/actions/address.js
+++ clientside/src/actions/address.js
@@ -5,6 +5,8 @@ export const getAddress = () => {
     return async (dispatch) => {
         try {
             const res = await axios.post(`/user/getAddress`);
+            console.log(res);
+
             dispatch({ type: addressConstants.GET_USER_ADDRESS_REQUEST });
             if (res.status === 200) {
                 const {
@@ -18,6 +20,35 @@ export const getAddress = () => {
                 const { error } = res.data;
                 dispatch({
                     type: addressConstants.GET_USER_ADDRESS_FAILURE,
+                    payload: { error },
+                });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+};
+
+export const addAddress = (payload) => {
+    return async (dispatch) => {
+        try {
+            const res = await axios.post(`/user/address/create`, { payload });
+            console.log(res);
+
+            dispatch({ type: addressConstants.ADD_USER_ADDRESS_REQUEST });
+            if (res.status === 201) {
+                console.log(res);
+                const {
+                    address: { address },
+                } = res.data;
+                dispatch({
+                    type: addressConstants.ADD_USER_ADDRESS_SUCCESS,
+                    payload: { address },
+                });
+            } else {
+                const { error } = res.data;
+                dispatch({
+                    type: addressConstants.ADD_USER_ADDRESS_FAILURE,
                     payload: { error },
                 });
             }
